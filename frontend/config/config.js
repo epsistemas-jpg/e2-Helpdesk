6a0092af41dcd83afea0f99aa0c73d330c7350fa
Sebasbehe <sebastianpersmor2006@gmail.com>
2026-07-24 16:13:21 -0500

    Configurar API de producción

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,11 +1,5 @@
 const CONFIG = {
-
-    API_URL: "http://localhost:4000/api",
-
-    APP_NAME: "E2 HelpDesk",
-
-    VERSION: "1.0.0"
-
+    API_URL: import.meta.env.VITE_API_URL
 };
 
 export default CONFIG;
